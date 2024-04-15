0a573c9fa62cd25450cc286b1008ce875149ad60
Jaya Sabeen <jayasabeen@Jayas-MacBook-Pro.local>
2024-04-15 15:52:54 +0530

    Created Empty Project

---
+++ PrashantAdvait/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PrashantAdvait
+//
+//  Created by Jaya Sabeen on 15/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PrashantAdvait/PrashantAdvaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrashantAdvaitApp.swift
+//  PrashantAdvait
+//
+//  Created by Jaya Sabeen on 15/04/24.
+//
+
+import SwiftUI
+
+@main
+struct PrashantAdvaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

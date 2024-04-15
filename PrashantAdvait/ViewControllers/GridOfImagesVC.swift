46d565aa598fb04d08e8605c70bea2cf07b58950
Jaya Sabeen <jayasabeen@Jayas-MacBook-Pro.local>
2024-04-15 16:16:52 +0530

    Added mock data for developing UI

---
+++ PrashantAdvait/ViewControllers/GridOfImagesVC.swift
@@ -0,0 +1,14 @@
+//
+//  GridOfImagesVC.swift
+//  PrashantAdvait
+//
+//  Created by Jaya Sabeen on 15/04/24.
+//
+
+import Foundation
+
+struct MockData {
+    static let data1 = MediaCoverage(id: "coverage-82b13a", title: "इस भ्रम में न रहें कि दुनिया आप चला रहें हैं", language: "hindi", thumbnail: MediaCoverage.Thumbnail(id: "img-c65af77b-94e8-47b5-8d57-0858500e8cd4", version: 1, domain: "https://cimg.acharyaprashant.org", basePath: "images/img-c65af77b-94e8-47b5-8d57-0858500e8cd4", key: "image.jpg", qualities: [10, 20, 30, 40], aspectRatio: 1), mediaType: 2, coverageURL: "https://cimg.acharyaprashant.org/images/img-6e619631-61f1-46aa-a311-aa7d0a11975b/40/image.jpg", publishedAt: "2024-03-02", publishedBy: "अमर उजाला", backupDetails:  nil)
+    static let data2 = MediaCoverage(id: "coverage-64b130", title: "From IIM to Monkhood: How an IIT-IIM alumnus found spiritual fulfillment", language: "english", thumbnail: MediaCoverage.Thumbnail(id: "img-3f66bfcc-4a56-41f7-b24f-23e4d5402e43", version: 1, domain: "https://cimg.acharyaprashant.org", basePath: "images/img-3f66bfcc-4a56-41f7-b24f-23e4d5402e43", key: "image.jpg", qualities: [10, 20, 30, 40], aspectRatio: 1), mediaType: 0, coverageURL: "https://www.indiatoday.in/education-today/news/story/from-iim-to-monkhood-how-an-iit-iim-alumnus-found-spiritual-fulfillment-acharya-prashant-2508011-2024-02-28", publishedAt: "2024-02-28", publishedBy: "India Today", backupDetails: MediaCoverage.BackupDetails(pdfLink: "https://paf-web.s3.ap-south-1.amazonaws.com/media/india-today-28-feb-2024.pdf", screenshotURL: "https://cimg.acharyaprashant.org/images/img-18cde857-119c-4b49-bc8a-a5b67226498f/40/image.jpg"))
+    static let Datas = [data1, data2]
+}

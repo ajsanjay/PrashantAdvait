e7435f7a55e66b353a5093375679f5644e8bf6dd
Jaya Sabeen <jayasabeen@Jayas-MacBook-Pro.local>
2024-04-15 15:56:33 +0530

    Git ignore added

--- PrashantAdvait/Dashboard.swift
+++ PrashantAdvait/Dashboard.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  Dashboard.swift
 //  PrashantAdvait
 //
 //  Created by Jaya Sabeen on 15/04/24.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct Dashboard: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    Dashboard()
 }
